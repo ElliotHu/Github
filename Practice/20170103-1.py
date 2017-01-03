e151fcb90fa9a49dd931f8299d76d82d0f23f4a0
Elliot.labPC <lxtylovewd@gaim.com>
2017-01-03 22:06:28 +0800

    2 commit

---
+++ Practice/20170103-1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  3 20:34:47 2017
+
+@author: Administrator
+"""
+
+"{0} love {1}.{2}".format("I","FishC","com")
+"{a} love {b}.{c}".format(a="I",b="FishC",c="com")
