af7fefebe27b89937f1209f042de24d1dcd41d91
Elliot.labPC <lxtylovewd@gaim.com>
2017-01-03 23:13:02 +0800

    3 commit

--- Practice/20170103-1.py
+++ Practice/20170103-1.py
@@ -7,3 +7,45 @@ Created on Tue Jan  3 20:34:47 2017
 
 "{0} love {1}.{2}".format("I","FishC","com")
 "{a} love {b}.{c}".format(a="I",b="FishC",c="com")
+
+a = list()
+a
+b= 'I love fishc.com'
+b = list(b)
+b
+c = (1, 2, 3, 4)
+c = list(c)
+c
+len(a)
+a
+len(b)
+b
+max(1,2,3,4,5)
+max(b)
+#max(b)
+#max(1,2,3,4,5)
+#max(b)
+#numbers = [1, 18, 13, 0, -34, 34, 76, 32]
+#max(numbers)
+#min
+#min(numbers)
+##chars = '1234567890'
+##min(chars)
+#tuple1 = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+#max(tuple1)
+##numbers.append('a')
+#numbers
+#max(numbers)
+#tuple2= (3.1, 3.2, 3.4)
+#sum(tuple2)
+#sorted(numbers)
+#numbers.pop()
+#
+#sorted(numbers)
+#reversed(numbers)
+#list(reversed(numbers))
+#list(enumerate(numbers))
+#a = [1, 2, 3, 4, 5, 6, 7, 8]
+#b = [4, 5, 6, 7, 8]
+#zip(a, b)
+#list(zip(a, b))
