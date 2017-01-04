b28c00c81a9d1cfb158549087930f0eedc1845b2
Elliot.labPC <lxtylovewd@gaim.com>
2017-01-04 23:28:41 +0800

    20170104practice

---
+++ Practice/myfirstfunction.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  4 23:14:20 2017
+
+@author: Administrator
+"""
+
+def MyFirstFunction():
+    print('这是我创建的第一个函数！')
+    print('我表示很激动')
+    print('在此我要感谢TVB，感谢CCAV，感谢你妹啊')
+
+MyFirstFunction()
+
+def MySecondFunction(name):
+    print(name + "我爱你！")
+MySecondFunction('小甲鱼')
+
+
+def add(num1,num2):
+    return ( num1+num2)
+print(add(5,6))
